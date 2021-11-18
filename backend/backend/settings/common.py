7278d20af6d944a32e280f71195db8bcb6c314fc
gyeongnamKim <rudska1885@gmail.com>
2021-11-18 16:36:26 +0900

    장고 - 포스팅 목록 구현 리액트 - 포스팅 목록 표현

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -25,9 +25,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'instagram',
+    'accounts',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -87,6 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -108,3 +114,9 @@ STATIC_ROOT = join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = join(BASE_DIR,'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
