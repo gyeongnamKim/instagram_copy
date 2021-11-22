0f7861fb28141c576372d3cce7f37d5b3ecf8018
gyeongnamKim <rudska1885@gmail.com>
2021-11-22 15:26:04 +0900

    리액트 - 포스팅 리스트에 jwt인증 구현

--- backend/instagram/views.py
+++ backend/instagram/views.py
@@ -1,11 +1,23 @@
+from datetime import timezone, timedelta
+
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 from .models import Post
 from .serializers import PostSerializer
+from django.db.models import Q
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [AllowAny] #FIXME: 인증적용
+    # permission_classes = [AllowAny] #FIXME: 인증적용
 
+    def get_queryset(self):
+        timesince = timezone.now() - timedelta(days=3)
+        qs = super().get_queryset()
+        qs= qs.filter(
+            Q(author=self.request.user) |
+            Q(author__in = self.request.user.following_set.all())
+        )
+        qs = qs.filter(created_at__gte=timesince)
+        return qs
