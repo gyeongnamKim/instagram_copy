f5d1a53f1b74a89412b759ddd3ddf7475809587b
gyeongnamKim <rudska1885@gmail.com>
2021-11-23 11:09:51 +0900

    리액트 - useAxios로 api 요청

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,38 +1,37 @@
 import React, { useEffect, useState } from "react";
 import "./SuggestionList.scss";
-import { Card } from "antd";
+import { Card, Button } from "antd";
 import Suggestion from "./Suggestion";
-import Axios from "axios";
+import useAxios from "axios-hooks";
 import { useAppContext } from "store";
 
 export default function SuggestionList({ style }) {
-  const [userList, setUserList] = useState([]);
   const {
     store: { jwtToken },
   } = useAppContext();
-  useEffect(() => {
-    async function fetchUserList() {
-      const apiUrl = "http://localhost:8000/accounts/suggestions/";
-      const headers = { Authorization: `JWT ${jwtToken}` };
-      try {
-        const { data } = await Axios.get(apiUrl, { headers });
-        setUserList(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchUserList();
+
+  const headers = { Authorization: `JWT ${jwtToken}` };
+
+  const [{ data: userList, loading, error }, refetch] = useAxios({
+    url: "http://localhost:8000/accounts/suggestions/",
+    headers,
   });
 
   return (
     <div style={style}>
+      {loading && <div>Loading...</div>}
+      {error && <div>로딩 중에 에러가 발생했습니다.</div>}
+
+      <Button onClick={() => refetch()}>Reload</Button>
+
       <Card title="Suggestions for you" size="small">
-        {userList.map((suggestionUser) => (
-          <Suggestion
-            key={suggestionUser.username}
-            suggestionUser={suggestionUser}
-          />
-        ))}
+        {userList &&
+          userList.map((suggestionUser) => (
+            <Suggestion
+              key={suggestionUser.username}
+              suggestionUser={suggestionUser}
+            />
+          ))}
       </Card>
     </div>
   );
